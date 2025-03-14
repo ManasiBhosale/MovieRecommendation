6218c940ea3936db0921738c24d463da917a1cfb
ManasiBhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-03-14 18:27:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 import subprocess
 import os
 import json
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Required for session handling
